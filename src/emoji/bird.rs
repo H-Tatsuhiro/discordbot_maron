5fd401bb91b1799b7a990bb08874c5f4b0d09a2e
H.Tatsuhiro <admin@h-tatsu.com>
2022-01-08 18:46:55 +0900

    `!emoji`コマンドに`bird`を実装（鳥の絵文字を返す機能）, `!emoji`コマンドのデフォルトを`bird`に変更, `!emoji`コマンドの存在しないエイリアスを入力した場合の挙動を実装

---
+++ src/emoji/bird.rs
@@ -0,0 +1,15 @@
+use serenity::framework::standard::{macros::command, Args, CommandResult};
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+#[command]
+pub async fn bird(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let content = if args.is_empty() {
+        ":bird:".to_string()
+    } else {
+        format!(":bird: '{}'という動物が見つからないよ！", args.rest())
+    };
+
+    msg.channel_id.say(&ctx.http, content).await?;
+    Ok(())
+}
--- src/emoji/emoji_struct.rs
+++ src/emoji/emoji_struct.rs
@@ -1,3 +1,4 @@
+use crate::emoji::bird::BIRD_COMMAND;
 use crate::emoji::cat::CAT_COMMAND;
 use crate::emoji::dog::DOG_COMMAND;
 use serenity::framework::standard::macros::group;
@@ -6,6 +7,6 @@ use serenity::framework::standard::macros::group;
 #[prefixes("emoji", "em")]
 #[description = "絵文字メッセージのコマンドグループ"]
 #[summary = "色々な絵文字を使えるよ"]
-#[default_command(dog)]
-#[commands(cat)]
+#[default_command(bird)]
+#[commands(cat, dog)]
 pub struct Emoji;
